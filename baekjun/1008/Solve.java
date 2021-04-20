1f868b467f84b5c9183cd6e4823ab1d078fe4724
Achelous1 <jsdesign1204@gmail.com>
2021-04-20 15:50:18 +0900

    ADD baekjun 1008 solution

---
+++ baekjun/1008/Solve.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solve {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] inputs = br.readLine().split(" ");
+        System.out.println(Double.valueOf(inputs[0]) / Double.valueOf(inputs[1]));
+
+    }
+}
